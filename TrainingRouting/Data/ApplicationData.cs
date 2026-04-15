1f764347cddca686eea48a72b7da287cb5139221
Łukasz Dziubiela <s33646@pjwstk.edu.pl>
2026-04-15 15:54:14 +0200

    Zmieniony Program.cs, dodalem wiecej danych w data.

--- TrainingRouting/Data/ApplicationData.cs
+++ TrainingRouting/Data/ApplicationData.cs
@@ -27,4 +27,30 @@ public class ApplicationData
             IsActive = true
         }
     ];
+
+    public static List<Reservation> Reservations { get; } =
+    [
+        new Reservation
+        {
+            Id = 1,
+            RoomId = 1,
+            OrganizerName = "Anna Kowalska",
+            Topic = "REST Basics",
+            Date = new DateOnly(2026, 5, 10),
+            StartTime = new TimeOnly(9, 0, 0),
+            EndTime = new TimeOnly(10, 30, 0),
+            Status = "confirmed"
+        },
+        new Reservation
+        {
+            Id = 2,
+            RoomId = 2,
+            OrganizerName = "Jan Nowak",
+            Topic = "ASP.NET Workshop",
+            Date = new DateOnly(2026, 5, 10),
+            StartTime = new TimeOnly(11, 0, 0),
+            EndTime = new TimeOnly(13, 0, 0),
+            Status = "planned"
+        }
+    ];
 }
