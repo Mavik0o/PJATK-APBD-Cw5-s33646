4795ab907d153177c5698c14807324dd9137ccfb
Łukasz Dziubiela <s33646@pjwstk.edu.pl>
2026-04-15 15:33:28 +0200

    Zrobiony model danych dla Room, Reservation

---
+++ TrainingRouting/Data/ApplicationData.cs
@@ -0,0 +1,30 @@
+﻿using TrainingRouting.Models;
+
+namespace TrainingRouting.Data;
+
+public class ApplicationData
+{
+    public static List<Room> Rooms { get; } =
+    [
+        new Room
+        {
+            Id = 1,
+            Name = "Room A101",
+            BuildingCode = "A",
+            Floor = 1,
+            Capacity = 20,
+            HasProjector = true,
+            IsActive = true
+        },
+        new Room
+        {
+            Id = 2,
+            Name = "Room A202",
+            BuildingCode = "A",
+            Floor = 2,
+            Capacity = 30,
+            HasProjector = false,
+            IsActive = true
+        }
+    ];
+}
